a05fbaea682a18a719d1061499fac0cc59c2be80
Ralo <74175604+ralo-med@users.noreply.github.com>
2023-04-14 17:09:10 +0900

    #3.2 Buttons Section

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,86 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Flutter!'),
-        ),
-        body: Center(
-          child: Text("Hello World!"),
-        ),
-      ),
-    );
+        home: Scaffold(
+            backgroundColor: Color(0xFF181818),
+            body: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 80,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                              'Hey, Ralo',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 28,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                            Text(
+                              'Welcome back',
+                              style: TextStyle(
+                                color: Colors.white.withOpacity(0.8),
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 120,
+                    ),
+                    Text(
+                      'Total Balance',
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.white.withOpacity(0.8),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      '\$5 194 582',
+                      style: TextStyle(
+                        fontSize: 48,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white.withOpacity(0.8),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                            decoration: BoxDecoration(
+                                color: Colors.amber,
+                                borderRadius: BorderRadius.circular(45)),
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(
+                                vertical: 20,
+                                horizontal: 50,
+                              ),
+                              child: Text(
+                                'Transfer',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ))
+                      ],
+                    )
+                  ],
+                ))));
   }
 }
